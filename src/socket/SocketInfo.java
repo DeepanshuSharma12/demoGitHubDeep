6a13bac42a61859a8e81f8bd840c887a24038b7b
deepanshu <deepanshu.iiita@outlook.com>
2017-07-31 21:53:52 +0530

    commit successfull

---
+++ src/socket/SocketInfo.java
@@ -0,0 +1,15 @@
+package socket;
+import java.io.*;  
+import java.net.*;  
+public class SocketInfo {  
+public static void main(String[] args) {  
+try{      
+Socket s=new Socket("localhost",8080);  
+DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+dout.writeUTF("Hello Server");  
+dout.flush();  
+dout.close();  
+s.close();  
+}catch(Exception e){System.out.println(e);}  
+}  
+}  
